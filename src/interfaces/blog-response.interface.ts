fde4ef09c13f88d37f99023e2d5b1015ee6cf201
Javier Vinueza <javiervin@hotmail.com>
2024-10-17 18:55:08 -0500

    feat: Add Tailwind integration to project configuration
    Added Tailwind integration to the project configuration file and updated dependencies accordingly.

---
+++ src/interfaces/blog-response.interface.ts
@@ -0,0 +1,126 @@
+export type BlogResponse = Post[];
+
+export interface Post {
+  id: number;
+  date: string;
+  date_gmt: string;
+  guid: Guid;
+  modified: string;
+  modified_gmt: string;
+  slug: string;
+  status: string;
+  type: string;
+  link: string;
+  title: Title;
+  content: Content;
+  excerpt: Excerpt;
+  author: number;
+  featured_media: number;
+  comment_status: string;
+  ping_status: string;
+  sticky: boolean;
+  template: string;
+  format: string;
+  meta: any[];
+  categories: number[];
+  tags: any[];
+  acf: Acf;
+  "mostrar-en-portal": string;
+  jetpack_featured_media_url: string;
+  _links: Links;
+}
+
+export interface Guid {
+  rendered: string;
+}
+
+export interface Title {
+  rendered: string;
+}
+
+export interface Content {
+  rendered: string;
+  protected: boolean;
+}
+
+export interface Excerpt {
+  rendered: string;
+  protected: boolean;
+}
+
+export interface Acf {
+  bp_en_portal: string;
+  bp_articulo_aprende: string;
+  reading_time_min: string;
+  bp_articulo_explora_ahorro: string;
+  bp_articulo_explora_ahorro_dolar: string;
+  bp_articulo_explora_ahorro_plus: string;
+  bp_articulo_explora_ahorro_dolar_plus: string;
+}
+
+export interface Links {
+  self: Self[];
+  collection: Collection[];
+  about: About[];
+  author: Author[];
+  replies: Reply[];
+  "version-history": VersionHistory[];
+  "predecessor-version": PredecessorVersion[];
+  "wp:featuredmedia": Featuredmedum[];
+  "wp:attachment": WpAttachment[];
+  "wp:term": WpTerm[];
+  curies: Cury[];
+}
+
+export interface Self {
+  href: string;
+}
+
+export interface Collection {
+  href: string;
+}
+
+export interface About {
+  href: string;
+}
+
+export interface Author {
+  embeddable: boolean;
+  href: string;
+}
+
+export interface Reply {
+  embeddable: boolean;
+  href: string;
+}
+
+export interface VersionHistory {
+  count: number;
+  href: string;
+}
+
+export interface PredecessorVersion {
+  id: number;
+  href: string;
+}
+
+export interface Featuredmedum {
+  embeddable: boolean;
+  href: string;
+}
+
+export interface WpAttachment {
+  href: string;
+}
+
+export interface WpTerm {
+  taxonomy: string;
+  embeddable: boolean;
+  href: string;
+}
+
+export interface Cury {
+  name: string;
+  href: string;
+  templated: boolean;
+}
